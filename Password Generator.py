b99c06c53830d1c4f3a2dbf3bbfa490636ad8f2c
thalluruPravallika <145014308+thalluruPravallika@users.noreply.github.com>
2023-09-17 14:37:15 +0530

    Create Password Generator.py

---
+++ Password Generator.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+from tkinter import messagebox
+import random
+import string
+
+def generate_password():
+    length = length_scale.get()
+    complexity = complexity_scale.get()
+
+    if length <= 0:
+        messagebox.showerror("Error", "Password length must be greater than zero.")
+        return
+
+    if complexity <= 0:
+        messagebox.showerror("Error", "Password complexity must be greater than zero.")
+        return
+
+    characters = ""
+    if complexity >= 1:
+        characters += string.ascii_letters
+    if complexity >= 2:
+        characters += string.digits
+    if complexity >= 3:
+        characters += string.punctuation
+
+    password = ''.join(random.choices(characters, k=length))
+    password_entry.delete(0, tk.END)
+    password_entry.insert(tk.END, password)
+
+def reset_password():
+    length_scale.set(8)
+    complexity_scale.set(1)
+    password_entry.delete(0, tk.END)
+
+root = tk.Tk()
+root.title("Password Generator")
+
+heading_label = tk.Label(root, text="Password Generator", font=("bold", 40))
+heading_label.pack(pady=10)
+
+user_label = tk.Label(root, text="User Name:",font=("bold", 36))
+user_label.pack()
+user_entry = tk.Entry(root)
+user_entry.pack(pady=5)
+
+length_label = tk.Label(root, text="Length:",font=("bold", 36))
+length_label.pack()
+length_scale = tk.Scale(root, from_=1, to=50, orient=tk.HORIZONTAL, length=200,bg="brown")
+length_scale.set(8)
+length_scale.pack(pady=5)
+
+complexity_label = tk.Label(root, text="Complexity:",font=("bold", 36))
+complexity_label.pack()
+complexity_scale = tk.Scale(root, from_=1, to=3, orient=tk.HORIZONTAL, length=100,bg="brown")
+complexity_scale.set(1)
+complexity_scale.pack(pady=5)
+
+generate_button = tk.Button(root, text="Generate Password",bg="Blue", command=generate_password)
+generate_button.pack(pady=10)
+
+password_label = tk.Label(root, text="Password:",font=("bold", 40))
+password_label.pack()
+password_entry = tk.Entry(root)
+password_entry.pack(pady=5)
+
+reset_button = tk.Button(root, text="Reset Password", command=reset_password,bg="purple")
+reset_button.pack(pady=10)
+
+root.mainloop()
+
